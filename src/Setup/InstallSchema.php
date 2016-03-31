b3656bd5a83390fcc0be20a3998d32b66cbd4994
Alex Gusev <alex@flancer64.com>
2016-03-31 14:07:51 +0300

    MOBI-265 - scheme setup migration

--- src/Setup/InstallSchema.php
+++ src/Setup/InstallSchema.php
@@ -7,12 +7,35 @@ namespace Praxigento\Odoo\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Praxigento\Odoo\Lib\Data\Entity\Lot;
+use Praxigento\Odoo\Lib\Data\Entity\Product;
+use Praxigento\Odoo\Lib\Data\Entity\Warehouse;
 
 class InstallSchema extends \Praxigento\Core\Setup\Schema\Base
 {
     protected function _setup(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        // TODO: Implement _setup() method.
+        /**
+         * Read and parse JSON schema.
+         */
+        $pathToFile = __DIR__ . '/../etc/dem.json';
+        $pathToNode = '/dBEAR/package/Praxigento/package/Odoo';
+        $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+
+        /* Product */
+        $entityAlias = Product::ENTITY_NAME;
+        $demEntity = $demPackage['entity']['Product'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
+
+        /* Lot */
+        $entityAlias = Lot::ENTITY_NAME;
+        $demEntity = $demPackage['entity']['Lot'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
+
+        /* Warehouse */
+        $entityAlias = Warehouse::ENTITY_NAME;
+        $demEntity = $demPackage['entity']['Warehouse'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
     }
 
 
