68c4fb6b4fabe3570abdd354b1adcc5c5d2a7efb
Alex Gusev <alex@flancer64.com>
2016-04-28 12:55:58 +0300

    MOBI-280 - Unit tests and code coverage after grids (repo in Downline)

--- src/Repo/Entity/Def/Category.php
+++ src/Repo/Entity/Def/Category.php
@@ -6,7 +6,7 @@ namespace Praxigento\Odoo\Repo\Entity\Def;
 
 use Magento\Framework\App\ResourceConnection;
 use Praxigento\Core\Repo\Def\Entity as BaseEntityRepo;
-use Praxigento\Core\Repo\IGeneric as IRepoBasic;
+use Praxigento\Core\Repo\IGeneric as IRepoGeneric;
 use Praxigento\Odoo\Data\Entity\Category as Entity;
 use Praxigento\Odoo\Repo\Entity\ICategory as IEntityRepo;
 
@@ -14,7 +14,7 @@ class Category extends BaseEntityRepo implements IEntityRepo
 {
     public function __construct(
         ResourceConnection $resource,
-        IRepoBasic $repoGeneric
+        IRepoGeneric $repoGeneric
     ) {
         parent::__construct($resource, $repoGeneric, Entity::class);
     }
