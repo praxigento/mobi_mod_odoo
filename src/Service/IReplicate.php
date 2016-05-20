c7c92cc8ced9b6fcf63327fb055cb35432ce35e5
Alex Gusev <alex@flancer64.com>
2016-05-20 15:21:53 +0300

    MOBI-292 - Product test data

--- src/Service/IReplicate.php
+++ src/Service/IReplicate.php
@@ -8,14 +8,19 @@ namespace Praxigento\Odoo\Service;
 interface IReplicate
 {
     /**
+     * Save products bundle ('push' replication).
+     *
      * @param Replicate\Request\ProductSave $req
      * @return Replicate\Response\ProductSave
      */
     public function productSave(Replicate\Request\ProductSave $req);
 
     /**
+     * Get products bundle from Odoo and save it ('pull' replication).
+     *
      * @param Replicate\Request\ProductsFromOdoo $req
      * @return Replicate\Response\ProductsFromOdoo
      */
     public function productsFromOdoo(Replicate\Request\ProductsFromOdoo $req);
+    
 }
