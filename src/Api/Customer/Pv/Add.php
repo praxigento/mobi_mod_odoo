33c381c3dd01b097179de7e061d6cb367023f125
Alex Gusev <alex@flancer64.com>
2016-12-22 17:57:25 +0200

    MOBI-516 - Send PV info from Odoo to Magento

--- src/Api/Customer/Pv/Add.php
+++ src/Api/Customer/Pv/Add.php
@@ -47,34 +47,47 @@ class Add
         if ($found) {
             $msg = "Odoo request referenced as '$odooRef' is already processed.";
             $this->logger->error($msg);
-            throw new \Exception($msg);
-        }
-        /* find customer by MLM ID */
-        $customer = $this->repoCustomer->getByMlmId($customerMlmId);
-        if (!$customer) {
-            $msg = "Customer #$customerMlmId is not found.";
-            $this->logger->error($msg);
-            throw new \Exception($msg);
-        }
-        /* add PV to account */
-        $req = new \Praxigento\Pv\Service\Transfer\Request\CreditToCustomer();
-        $req->setToCustomerId($customer->getCustomerId());
-        $req->setValue($pv);
-        $req->setDateApplied($dateApplied);
-        $note = "PV is added from Odoo (ref. #$odooRef).";
-        $req->setNoteOperation($note);
-        $req->setNoteTransaction($note);
-        $resp = $this->callPvTransfer->creditToCustomer($req);
-        if ($resp->isSucceed()) {
-            $this->repoRegRequest->create($key);
-            /* compose response */
-            $result->setOdooRef($odooRef);
-            $result->setOperationId($resp->getOperationId());
-            $transIds = $resp->getTransactionsIds();
-            $oneId = reset($transIds);
-            $result->setTransactionId($oneId);
-            $msg = "$pv PV are credit to customer #$customerMlmId (odoo ref. #$odooRef).";
-            $this->logger->info();
+            $result->getResult()->setCode($result::CODE_DUPLICATED);
+            $result->getResult()->setText($msg);
+        } else {
+            /* find customer by MLM ID */
+            $customer = $this->repoCustomer->getByMlmId($customerMlmId);
+            if (!$customer) {
+                $msg = "Customer #$customerMlmId is not found.";
+                $this->logger->error($msg);
+                $result->getResult()->setCode($result::CODE_CUSTOMER_IS_NOT_FOUND);
+                $result->getResult()->setText($msg);
+            } else {
+                try {
+                    /* add PV to account */
+                    $req = new \Praxigento\Pv\Service\Transfer\Request\CreditToCustomer();
+                    $req->setToCustomerId($customer->getCustomerId());
+                    $req->setValue($pv);
+                    $req->setDateApplied($dateApplied);
+                    $note = "PV is added from Odoo (ref. #$odooRef).";
+                    $req->setNoteOperation($note);
+                    $req->setNoteTransaction($note);
+                    $resp = $this->callPvTransfer->creditToCustomer($req);
+                    if ($resp->isSucceed()) {
+                        $this->repoRegRequest->create($key);
+                        /* compose response */
+                        $respData = new \Praxigento\Odoo\Api\Data\Customer\Pv\Add\Response\Data();
+                        $respData->setOdooRef($odooRef);
+                        $respData->setOperationId($resp->getOperationId());
+                        $transIds = $resp->getTransactionsIds();
+                        $oneId = reset($transIds);
+                        $respData->setTransactionId($oneId);
+                        $result->setData($respData);
+                        $result->getResult()->setCode($result::CODE_SUCCESS);
+                        $msg = "$pv PV are credit to customer #$customerMlmId (odoo ref. #$odooRef).";
+                        $this->logger->info($msg);
+                    }
+                } catch (\Throwable $e) {
+                    $msg = $e->getMessage();
+                    $msg .= "\n" . $e->getTraceAsString();
+                    $result->getResult()->setText($msg);
+                }
+            }
         }
         return $result;
     }
--- src/Api/Data/Customer/Pv/Add/Response.php
+++ src/Api/Data/Customer/Pv/Add/Response.php
@@ -7,12 +7,15 @@ namespace Praxigento\Odoo\Api\Data\Customer\Pv\Add;
 class Response
     extends \Praxigento\Core\Api\Response
 {
+    const CODE_CUSTOMER_IS_NOT_FOUND = 'CUSTOMER_IS_NOT_FOUND';
+    const CODE_DUPLICATED = 'DUPLICATED';
+
     /**
      * @return \Praxigento\Odoo\Api\Data\Customer\Pv\Add\Response\Data|null
      */
     public function getData()
     {
-        $result = parent::get();
+        $result = parent::get(self::ATTR_DATA);
         return $result;
     }
 
@@ -21,7 +24,7 @@ class Response
      */
     public function setData($data)
     {
-        parent::set($data);
+        parent::set(self::ATTR_DATA, $data);
     }
 
 }
