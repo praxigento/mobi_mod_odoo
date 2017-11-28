6882669b6ff46f5f47e00d1c040dd8555a214555
Alex Gusev <alex@flancer64.com>
2017-11-28 21:00:51 +0200

    MOBI-1012 Clean up "Praxigento\Core\Api" namespace from application level classes

--- src/Api/Data/Customer/Pv/Add/Response.php
+++ src/Api/Data/Customer/Pv/Add/Response.php
@@ -5,7 +5,7 @@
 namespace Praxigento\Odoo\Api\Data\Customer\Pv\Add;
 
 class Response
-    extends \Praxigento\Core\Api\Response
+    extends \Praxigento\Core\App\WebApi\Response
 {
     const CODE_CUSTOMER_IS_NOT_FOUND = 'CUSTOMER_IS_NOT_FOUND';
     const CODE_DUPLICATED = 'DUPLICATED';
