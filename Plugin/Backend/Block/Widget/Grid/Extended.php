f2c9a9c04f9529d160f4a10a8a3b0cb8995c413a
Alex Gusev <alex@flancer64.com>
2018-07-16 15:10:31 +0300

    MOBI-1542 No info in customers Orders tab

---
+++ Plugin/Backend/Block/Widget/Grid/Extended.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2018
+ */
+
+namespace Praxigento\Odoo\Plugin\Backend\Block\Widget\Grid;
+
+class Extended
+{
+    public function beforeSetCollection(
+        \Magento\Backend\Block\Widget\Grid\Extended $subject,
+        \Magento\Framework\Data\Collection $collection
+    ) {
+        /* MOBI-1542: prevent "Column 'customer_id' in where clause is ambiguous" */
+        /**
+         * @var \Magento\Sales\Model\ResourceModel\Order\Grid\Collection $collection
+         * @var \Magento\Framework\DB\Select $query
+         */
+        $query = $collection->getSelect();
+        if ($query) {
+            $where = $query->getPart(\Magento\Framework\DB\Select::WHERE);
+            foreach ($where as $key => $one) {
+                $replaced = str_replace('(`customer_id`', '(`main_table`.`customer_id`', $one);
+                $where[$key] = $replaced;
+            }
+            $query->setPart(\Magento\Framework\DB\Select::WHERE, $where);
+        }
+
+        return [$collection];
+    }
+}
