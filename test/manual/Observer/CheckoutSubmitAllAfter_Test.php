7a7d8bb749d820a0220e880ccb9cc5c26034cb89
Alex Gusev <alex@flancer64.com>
2018-09-18 18:03:19 +0300

    SAN-335 Last updated time in distributors dashboard September 7

--- test/manual/Observer/CheckoutSubmitAllAfter_Test.php
+++ test/manual/Observer/CheckoutSubmitAllAfter_Test.php
@@ -8,7 +8,8 @@ use Magento\Framework\App\ObjectManager;
 
 include_once(__DIR__ . '/../phpunit_bootstrap.php');
 
-class CheckoutSubmitAllAfter_ManualTest extends \Praxigento\Core\Test\BaseIntegrationTest
+class CheckoutSubmitAllAfter_ManualTest
+    extends \Praxigento\Core\Test\BaseCase\Manual
 {
     /** @var  CheckoutSubmitAllAfter */
     private $obj;
