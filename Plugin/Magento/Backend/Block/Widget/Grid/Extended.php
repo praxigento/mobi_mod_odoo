a85c5cf6ac02ec28e3949db6ac7c1f456b2e9309
Alex Gusev <alex@flancer64.com>
2018-10-14 11:41:58 +0300

    MOBI-1577 Magento reports don’t load

--- Plugin/Magento/Backend/Block/Widget/Grid/Extended.php
+++ Plugin/Magento/Backend/Block/Widget/Grid/Extended.php
@@ -13,20 +13,20 @@ class Extended
         \Magento\Framework\Data\Collection $collection
     ) {
         /* MOBI-1542: prevent "Column 'customer_id' in where clause is ambiguous" */
-        /**
-         * @var \Magento\Sales\Model\ResourceModel\Order\Grid\Collection $collection
-         * @var \Magento\Framework\DB\Select $query
-         */
-        $query = $collection->getSelect();
-        if ($query) {
-            $where = $query->getPart(\Magento\Framework\DB\Select::WHERE);
-            foreach ($where as $key => $one) {
-                $replaced = str_replace('(`customer_id`', '(`main_table`.`customer_id`', $one);
-                $where[$key] = $replaced;
+        if ($collection instanceof \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection) {
+            /**
+             * @var \Magento\Framework\DB\Select $query
+             */
+            $query = $collection->getSelect();
+            if ($query) {
+                $where = $query->getPart(\Magento\Framework\DB\Select::WHERE);
+                foreach ($where as $key => $one) {
+                    $replaced = str_replace('(`customer_id`', '(`main_table`.`customer_id`', $one);
+                    $where[$key] = $replaced;
+                }
+                $query->setPart(\Magento\Framework\DB\Select::WHERE, $where);
             }
-            $query->setPart(\Magento\Framework\DB\Select::WHERE, $where);
         }
-
         return [$collection];
     }
 }
