6a85fa19f923f05bb8aa1d67f11d8d73f6ab20a6
Alex Gusev <alex@flancer64.com>
2018-11-05 16:05:09 +0200

    MOBI-1610 Product & orders sync UI interface

--- Config.php
+++ Config.php
@@ -13,14 +13,20 @@ use Praxigento\Warehouse\Config as WrhsCfg;
 class Config
     extends \Praxigento\Accounting\Config
 {
+    /** see ./mobi_mod_odoo/etc/acl.xml (resource-nodes ids) */
     const ACL_CATALOG_LOTS = WrhsCfg::ACL_CATALOG_LOTS;
     const ACL_CATALOG_WAREHOUSES = WrhsCfg::ACL_CATALOG_WAREHOUSES;
+    const ACL_ODOO = 'odoo';
+    const ACL_REPLICATE = 'replicate';
 
     const CODE_TYPE_ASSET_PV = PvCfg::CODE_TYPE_ASSET_PV;
     const CODE_TYPE_ASSET_WALLET_ACTIVE = WalletCfg::CODE_TYPE_ASSET_WALLET;
     const CODE_TYPE_OPER_WALLET_DEBIT = 'ODOO_DEBIT';
 
+    /** see ./mobi_mod_odoo/etc/adminhtml/menu.xml (add-nodes ids) */
     const MENU_CATALOG_LOTS = self::ACL_CATALOG_LOTS;
+    const MENU_REPLICATE_PRODUCTS = 'replicate_products';
+    const MENU_REPLICATE_ORDERS = 'replicate_sales';
     const MENU_CATALOG_WAREHOUSES = self::ACL_CATALOG_WAREHOUSES;
 
     const MODULE = 'Praxigento_Odoo';
